73b2205004e5bac8f1ff6a9c040043f7b519969c
steph37tours <stephane.curet@dev-tec.fr>
2024-02-10 21:20:55 +0100

    complément

--- pairImpair.py
+++ pairImpair.py
@@ -1,10 +1,10 @@
 liste = [1,2,3,8,10,13,56,7]
-pair=[]
-impair=[]
+PAIR=[]
+IMPAIR=[]
 for i in liste:
     if i%2 == 0:
         pair.append(i)
     else :
         impair.append(i)
-print('Voici la liste des pairs ',pair)
-print('Voici la liste des impairs ',impair)
+print('Voici la liste des pairs ',PAIR)
+print('Voici la liste des impairs ',IMPAIR)
