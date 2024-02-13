b641c90f3e206ae1e5ee0da6448453f92e37798c
steph37tours <stephane.curet@dev-tec.fr>
2024-02-13 22:41:35 +0100

    grille et polynome

---
+++ grille.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    for k in range (20):
+        print('X',end='')
+    print('')
--- polynome.py
+++ polynome.py
@@ -1,4 +1,15 @@
-for k in range (3) :
-    x= float(input('Quelle est la valeur de X ?'))
-    polynome = 3 * x*x + 5 *x + 2
-    print ('Le ',k,' résultat est', polynome)
+import math
+a = int(input('entrez a'))
+b = int(input('entrez b'))
+c = int(input('entrez c'))
+Delta = b**2 - 4 *a *c
+if Delta > 0 :
+    sol1 = (- b + math.sqrt(Delta)) / 2 * a
+    sol2 = (-b - math.sqrt(Delta) )/ 2 * a
+    print ('Les solutions sont ', sol1, ' et ', sol2)
+else :
+    if Delta == 0 :
+        sol1 = -b /2 * a
+        print ('la solution est ', sol1)
+    else :
+        print ('Pas de solution')
