8eeff5fd3573819582926ad8b7510debc4ff17e4
steph37tours <stephane.curet@dev-tec.fr>
2024-02-15 20:59:52 +0100

    nb premier

---
+++ nbPremier.py
@@ -0,0 +1,8 @@
+#trouver les nb premiers dans les 100 premiers nombres
+for k in range (1,100+1):
+    premier = True
+    for i in range(2,k-1):
+        if (k%i)==0:
+            premier = False
+    if premier== True :
+        print(k , end='')
