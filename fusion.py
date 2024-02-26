dfb44dfe7e03c7cf7c8b63f838470d8b2109d9d1
steph37tours <stephane.curet@dev-tec.fr>
2024-02-26 21:37:05 +0100

    fusion

--- fusion.py
+++ fusion.py
@@ -1,10 +1,16 @@
 Liste1 = [4,5,9]
-Liste2 = [1,6,10]
+Liste2 = [1,6,10,15]
 Liste3 = []
+ll1 = len(Liste1)
+ll2 = len(Liste2)
 i = 0
 j = 0
 while True:
-    if ( i>= 3 & j >= 3) :
+    if ( i>= ll1) :
+        Liste3.append(Liste2[j:])
+        break
+    if (j >= ll2):
+        Liste3.append(Liste1[i:])
         break
     if(Liste1[i]<=Liste2[j]):
         Liste3.append(Liste1[i])
